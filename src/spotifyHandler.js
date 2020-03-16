b91946993db917ebf68558d006a925c059059424
Alexander Schlicker <alexander@schlickster.us>
2020-03-16 14:54:57 -0400

    some basic error handling for librespot

--- src/spotifyHandler.js
+++ src/spotifyHandler.js
@@ -5,18 +5,24 @@ class SpotifyHandler {
     constructor(path, name = 'PiNetwork'){
         this.name = name;
         this.path = path;
-    }
-  
-    start() {
-        this.forever = Forever.start(['librespot'], {
+
+        this.child = new (Forever.Monitor)(['librespot'], {
             cwd: this.path,
             command: 'librespot',
             args: ['--name', this.name, '-v']
         });
+
+        this.child.on("error", error => {
+            console.error("There was an error starting librespot", error);
+        })
+    }
+  
+    start() {
+        this.child.start();
     }
 
     stop() {
-        this.forever.stop();
+        this.child.stop();
     }
 }
 
